db262e78181445fc9fe6a42d8de5d04dfa905b39
Mohammad Ahmad <mohammad-md-ahmad@github.com>
2023-10-15 17:44:18 +0300

    axios timeout and error handling, changing api url

--- mobile-app/Liber/api/AuthService.tsx
+++ mobile-app/Liber/api/AuthService.tsx
@@ -5,8 +5,6 @@ class AuthService extends AxiosService {
     async login(username: string, password: string) {
         return this.post('/login', { username, password });
     }
-
 }
 
 export default AuthService;
-
--- mobile-app/Liber/helpers/constants.tsx
+++ mobile-app/Liber/helpers/constants.tsx
@@ -1,6 +1,6 @@
 const Constants = {
-    apiUrl: 'http://192.168.0.105:81/api',
+    apiUrl: 'http://liber.quad-bh.com/api',
     tokenKey: 'auth_token'
 };
-export default Constants;
 
+export default Constants;
