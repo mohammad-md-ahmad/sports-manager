dfcbad54c8640836f93450e6dde08bc73b8d59b0
Mohamed Mustafa <mohamed.j.mustafa@gmail.com>
2023-10-19 00:44:41 +0300

    profile forms fixes

--- mobile-app/Liber/api/CompanyService.tsx
+++ mobile-app/Liber/api/CompanyService.tsx
@@ -3,11 +3,11 @@ import AxiosService from './AxiosService';
 class CompanyService extends AxiosService {
 
     async create(data: Object) {
-        return this.post('/companies',data);
+        return this.post('/companies', data);
     }
 
     async update(data: Object) {
-        return this.put('/companies',data);
+        return this.put('/companies/' + data.uuid, data);
     }
 }
 
--- mobile-app/Liber/api/UserService.tsx
+++ mobile-app/Liber/api/UserService.tsx
@@ -3,11 +3,11 @@ import AxiosService from './AxiosService';
 class UserService extends AxiosService {
 
     async create(data: Object) {
-        return this.post('/users',data);
+        return this.post('/users', data);
     }
 
     async update(data: Object) {
-        return this.put('/users',data);
+        return this.put('/users/' + data.uuid, data);
     }
 }
 
