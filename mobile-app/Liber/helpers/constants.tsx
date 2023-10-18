4e7a3b653cc737e0be3b333bf9909702f27f577e
Mohamed Mustafa <mohamed.j.mustafa@gmail.com>
2023-10-19 00:24:12 +0300

    constants

--- mobile-app/Liber/helpers/constants.tsx
+++ mobile-app/Liber/helpers/constants.tsx
@@ -1,6 +1,5 @@
 const Constants = {
-    apiUrl: 'http://192.168.0.105:81/api',
-   // apiUrl: 'http://liber.quad-bh.com/api',
+    apiUrl: 'http://liber.quad-bh.com/api',
     tokenKey: 'auth_token',
     userDataKey: 'user_data',
 };
