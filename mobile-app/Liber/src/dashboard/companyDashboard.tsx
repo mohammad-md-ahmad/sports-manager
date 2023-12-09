e8a50846dddf954a7bbc5f47878dee9f2c1ae565
Mohamed Mustafa <mohamed.j.mustafa@gmail.com>
2023-12-10 00:53:42 +0200

    - several UI fixes

--- mobile-app/Liber/src/dashboard/companyDashboard.tsx
+++ mobile-app/Liber/src/dashboard/companyDashboard.tsx
@@ -6,90 +6,72 @@ import {
     StyleSheet,
     Text,
     View,
+    VirtualizedList,
 } from "react-native";
 import colors from "../../styles/colors";
-import { Card } from "react-native-elements";
+import FacilityCard from "../common/facilityCard";
+import FacilityService from "../../api/FacilityService";
+import MiscService from "../../api/MiscService";
+import { useFocusEffect } from "@react-navigation/native";
+import { storeFacilityTypes } from "../../helpers/facilityTypesDataManage";
+import { storeCountries } from "../../helpers/countriesDataManage";
 
 export default function CompanyDashboard(): React.JSX.Element {
+    const facilityService = new FacilityService();
+    const miscService = new MiscService();
+
+    const [facilities, setFacilities] = useState([]);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            // This code will execute when the component gains focus (navigated to).
+            // You can put the logic here that you want to run when the component should reload.
+
+            facilityService.list({})
+                .then((response) => {
+                    setFacilities(response.data?.data.data);
+                }).catch((error) => {
+                });
+
+            miscService.lists().then((response) => {
+                storeFacilityTypes(response.data?.data?.facility_types);
+                storeCountries(response.data?.data?.countries);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }, [])
+    );
+
+    const getItem = (_data: unknown, index: number) => facilities[index];
+    const getItemCount = (_data: unknown) => facilities.length;
 
     return (
-        <SafeAreaView style={styles.containerView}>
-            <ScrollView>
-                <Card containerStyle={styles.cardView}>
-                    <Card.Title>Dashboard Widget 1</Card.Title>
-
-                    <Card.Divider />
-                    <View style={{ position: "relative", alignItems: "center" }}>
-                        <Text >Details </Text>
-                    </View>
-                    <View style={styles.container}>
-
-
-                    </View>
-                </Card>
-
-                <Card containerStyle={styles.cardView}>
-                    <Card.Title>Dashboard Widget 2</Card.Title>
-
-                    <Card.Divider />
-                    <View style={{ position: "relative", alignItems: "center" }}>
-                        <Text >Details </Text>
-                    </View>
-                    <View style={styles.container}>
-
-
-                    </View>
-                </Card>
-
-                <Card containerStyle={styles.cardView}>
-                    <Card.Title>Dashboard Widget 3</Card.Title>
-
-                    <Card.Divider />
-                    <View style={{ position: "relative", alignItems: "center" }}>
-                        <Text >Details </Text>
-                    </View>
-                    <View style={styles.container}>
-
-
-                    </View>
-                </Card>
-
-                <Card containerStyle={styles.cardView}>
-                    <View style={styles.container}>
-
-                        <Card.Title>Dashboard Widget 4</Card.Title>
-
-                        <Card.Divider />
-                        <View style={{ position: "relative", alignItems: "center" }}>
-                            <Text >Details </Text>
-                        </View>
-                        <View style={styles.container}>
-
-
-                        </View>
-                    </View>
-                </Card>
-            </ScrollView>
+        <SafeAreaView style={styles.container}>
+            <VirtualizedList
+                initialNumToRender={6}
+                renderItem={({ item }) => <FacilityCard facility={item} />}
+                keyExtractor={item => item.uuid}
+                getItemCount={getItemCount}
+                getItem={getItem}
+            />
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    containerView: {
+    container: {
         flex: 1,
         marginVertical: 0,
 
     },
-    cardView: {
-        borderRadius: 10,
-        borderColor: colors.PrimaryGreenLight,
-        borderWidth: 0.5,
-        marginHorizontal: 15,
-        marginTop: 7,
-        marginBottom: 7
+    item: {
+        backgroundColor: colors.PrimaryBlue,
+        height: 150,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        padding: 20,
     },
-    container: {
-        height: 200
+    title: {
+        fontSize: 32,
     },
-
 });
