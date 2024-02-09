778809dacce8c9cf111172d34e640a798839fbe8
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-02-10 01:53:13 +0300

    sports service - done

--- app/Services/Data/Sport/CreateSportRequest.php
+++ app/Services/Data/Sport/CreateSportRequest.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Services\Data\Sport;
 
-use App\Models\Advertisement;
+use App\Models\Sport;
 use Spatie\LaravelData\Attributes\Validation\Unique;
 use Spatie\LaravelData\Data;
 
 class CreateSportRequest extends Data
 {
     public function __construct(
-        #[Unique(Advertisement::class)]
+        #[Unique(Sport::class)]
         public string $name,
         public ?string $description = null,
         public ?string $icon = null,
--- app/Services/SportService.php
+++ app/Services/SportService.php
@@ -5,27 +5,30 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Contracts\Services\SportServiceInterface;
-use App\Models\Advertisement;
 use App\Models\Sport;
 use App\Services\Data\Sport\CreateSportRequest;
 use App\Services\Data\Sport\DeleteSportRequest;
 use App\Services\Data\Sport\GetAllSportsRequest;
 use App\Services\Data\Sport\GetSportRequest;
 use App\Services\Data\Sport\UpdateSportRequest;
+use App\Traits\ImageUpload;
 use Exception;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class SportService implements SportServiceInterface
 {
+    use ImageUpload;
+
     /**
      * @throws Exception
      */
     public function getAll(GetAllSportsRequest $data): LengthAwarePaginator
     {
         try {
-            $sports = Advertisement::query()->jsonPaginate();
+            $sports = Sport::query()->jsonPaginate();
 
             return $sports;
         } catch (Exception $exception) {
@@ -58,10 +61,19 @@ class SportService implements SportServiceInterface
     public function store(CreateSportRequest $data): Sport
     {
         try {
+            $dataWithoutIcon = $data->toArray();
+            unset($dataWithoutIcon['icon']);
+
             DB::beginTransaction();
 
             /** @var Sport $sport */
-            $sport = Sport::create($data->toArray());
+            $sport = Sport::create($dataWithoutIcon);
+
+            if ($data->icon) {
+                $uploadedImg = $this->uploadImage($data->icon, $sport->id);
+                $sport->icon = $uploadedImg;
+                $sport->save();
+            }
 
             DB::commit();
 
@@ -86,9 +98,18 @@ class SportService implements SportServiceInterface
             /** @var Sport $sport */
             $sport = Sport::findOrFail($data->id);
 
+            $dataWithoutIcon = $data->toArray();
+            unset($dataWithoutIcon['icon']);
+
             DB::beginTransaction();
 
-            $sport->update($data->toArray());
+            $sport->update($dataWithoutIcon);
+
+            if ($data->icon) {
+                $uploadedImg = $this->uploadImage($data->icon, $sport->id);
+                $sport->icon = $uploadedImg;
+                $sport->save();
+            }
 
             DB::commit();
 
@@ -114,13 +135,52 @@ class SportService implements SportServiceInterface
             $sport = Sport::findOrFail($data->id);
 
             DB::transaction(function () use ($sport) {
-                //                $advertisement->gallery()->delete();
                 $sport->delete();
             });
 
             return true;
         } catch (Exception $exception) {
             Log::error('SportService::delete: '.$exception->getMessage());
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function uploadImage(string $image, int|string $id = null): string
+    {
+        try {
+            do {
+                $imageData = $this->base64Decode($image);
+                $path = $this->base64ToImage($image, 'sports', $imageData);
+            } while (! Storage::disk('public')->put($path, $imageData));
+
+            // delete the old icon
+            if ($id) {
+                /** @var Sport $sport */
+                $sport = Sport::findOrFail($id);
+
+                if ($sport->icon) {
+                    $this->deleteImage($sport->icon);
+                }
+            }
+
+            return $path;
+        } catch (Exception $exception) {
+            Log::error('Uploading sport icon: '.$exception->getMessage());
+
+            throw $exception;
+        }
+    }
+
+    private function deleteImage(string $image): bool
+    {
+        try {
+            return Storage::disk('public')->delete($image);
+        } catch (Exception $exception) {
+            Log::error('Delete sport icon: '.$exception->getMessage());
 
             throw $exception;
         }
