d9c04e4c45dd6337bd2db04b640f85f86c5ab02e
Mohammad Ahmad <mohammad.ahmad@mdgroup.com>
2024-02-11 13:44:13 +0300

    fixing route typo

--- routes/api.php
+++ routes/api.php
@@ -89,7 +89,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
             Route::prefix('surveys')->group(function () {
 
                 Route::post('/', [CompanySurveyController::class, 'store'])->name('company-surveys.create');
-                Route::post('/send-survey', [CompanySurveyController::class, 'sendSurvey'])->name('company-surveys.send');
+                Route::post('/send', [CompanySurveyController::class, 'sendSurvey'])->name('company-surveys.send');
             });
         });
     });
