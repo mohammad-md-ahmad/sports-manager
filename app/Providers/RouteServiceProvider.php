0d781b15b06a0386c6fa74251a361ed8075c3216
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-02-17 23:13:19 +0300

    adding separate routes for web api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,10 +31,18 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
+        RateLimiter::for('web-api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('web-api')
+                ->prefix('web-api')
+                ->group(base_path('routes/web-api.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
