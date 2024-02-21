ccc745b5a377688161c773b2a176de6d7d94100b
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-02-22 00:27:50 +0300

    next configs

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  reactStrictMode: false
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: false,
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
