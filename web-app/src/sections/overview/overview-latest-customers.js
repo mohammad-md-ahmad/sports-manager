b37c4bbe1cd6500e34ab5190c1f7b7f8398daae1
Mohamed Mustafa <mohamed.j.mustafa@gmail.com>
2024-02-21 23:38:59 +0200

    - dashboard widgets

--- web-app/src/sections/overview/overview-latest-customers.js
+++ web-app/src/sections/overview/overview-latest-customers.js
@@ -20,17 +20,36 @@ import {
 import { Scrollbar } from 'src/components/scrollbar';
 import { SeverityPill } from 'src/components/severity-pill';
 import { DataGrid, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid';
-import { CompanyStatus } from 'helpers/constants';
+import { CompanyStatus, UserType } from 'helpers/constants';
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import { useRouter } from 'next/router';
 import CompanyService from 'api/CompanyService';
+import UserService from 'api/UserService';
+import { useEffect, useState } from 'react';
 
-export const OverviewPendingApprovalCompanies = (props) => {
-  const { companies = [], loadData, sx } = props;
+export const OverviewLatestCustomers = (props) => {
   const router = useRouter();
+  const userService = new UserService();
+  const [customers, setCustomer] = useState([]);
 
-  const companyService = new CompanyService();
+  const { sx } = props;
+
+  useEffect(() => {
+    loadData();
+  }, [])
+
+  const loadData = () => {
+    userService.list({ type: UserType.CustomerUser }).then((response) => {
+      setCustomer(response?.data?.data?.data)
+    }).catch((error) => {
+      // Handle API request errors here
+      console.error(error);
+      //throw new Error('Please check your email and password');
+      throw new Error(error.message);
+    });
+  }
+
 
   function ImageCell(params) {
     return (
@@ -46,68 +65,39 @@ export const OverviewPendingApprovalCompanies = (props) => {
 
   const columns = [
     {
-      field: 'logo',
-      headerName: 'Logo',
+      field: 'profile_picture',
+      headerName: 'Photo',
       width: 100,
       renderCell: ImageCell
     },
     {
-      field: 'name',
+      field: 'full_name',
       headerName: 'Name',
       flex: 1
-    },
-    {
-      field: 'status',
-      type: 'actions',
-      headerName: 'Approve',
-      width: 140,
-      cellClassName: 'status',
-      getActions: (params) => {
-        return [
-          params?.row?.status == CompanyStatus.PendingApproval ?
-            <IconButton color="success" onClick={() => approveCompany(params?.row?.uuid)}>
-              <CheckIcon />
-            </IconButton>
-            :
-            <></>
-        ];
-      }
     }
   ];
 
-  const approveCompany = (uuid) => {
-    companyService.update({ uuid, status: CompanyStatus.Active }).then((response) => {
-      loadData();
-    }).catch((error) => {
-      // Handle API request errors here
-      console.error(error);
-      //throw new Error('Please check your email and password');
-      throw new Error(error.message);
-    });
-  }
 
   const handleRowClick = (params) => {
     // Handle the row click event
     console.log('Row clicked:', params.row);
     // You can perform actions based on the clicked row, such as navigating to a detail page
 
-    router.push('/company?id=' + params.row.uuid);
+    router.push('/customer?id=' + params.row.uuid);
   };
 
   const viewAll = () => {
-    router.push('/companies');
+    router.push('/customers');
   };
-
-
 
   return (
     <Card sx={sx}>
-      <CardHeader title="Pending Approval Companies" />
+      <CardHeader title="Latest Customers" />
       <Scrollbar sx={{ flexGrow: 1 }}>
         <Box sx={{ maxWidth: '90%', marginLeft: '5%' }}>
           <DataGrid
             getRowId={(row) => row.uuid}
-            rows={companies}
+            rows={customers}
             columns={columns}
             initialState={{
               pagination: {
@@ -116,7 +106,7 @@ export const OverviewPendingApprovalCompanies = (props) => {
             }}
             pageSizeOptions={[5, 10]}
             disableRowSelectionOnClick
-          
+
             slotProps={{
               toolbar: {
                 showQuickFilter: true,
@@ -148,7 +138,7 @@ export const OverviewPendingApprovalCompanies = (props) => {
   );
 };
 
-OverviewPendingApprovalCompanies.prototype = {
+OverviewLatestCustomers.prototype = {
   orders: PropTypes.array,
   sx: PropTypes.object
 };
