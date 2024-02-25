e2fdacdf48622365f4afed3d16f3e6bf03cfaada
mohammad ahmad <mohammad.h.ahmad@gmail.com>
2024-02-25 13:46:45 +0300

    configs

---
+++ web-app/ImageLoader.js
@@ -0,0 +1,3 @@
+export default function ImageLoader({ src }) {
+  return `https://liber.quad-bh.com/admin-portal/${src}`;
+}
--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,18 +1,26 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  reactStrictMode: false,
-  output: 'export',
+    reactStrictMode: false,
+    output: 'export',
+    basePath: '/admin-portal',
+    assetPrefix: isProd ? 'https://liber.quad-bh.com/admin-portal/' : undefined,
+    images: {
+        loader: "custom",
+        loaderFile: './ImageLoader.js'
+    },
 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
 }
 
 module.exports = nextConfig
